204a5a38cb375ac23f23c650edf7b1ab0f9a924a
lannaj <ljmack@seas.upenn.edu>
2019-11-07 19:38:46 -0500

    card class for blackjack

--- Card.java
+++ Card.java
@@ -78,25 +78,17 @@ public class Card {
 		return rank;
 	}
 
-	public void setRank(String rank) {
-		this.rank = rank;
-	}
 
 	public String getSuit() {
 		return suit;
 	}
 
-	public void setSuit(String suit) {
-		this.suit = suit;
-	}
 
 	public int getValue() {
 		return value;
 	}
 
-	public void setValue(int value) {
-		this.value = value;
-	}
+
 
 	
 }
